74e8e70ed82ee9e5324eaa2eeaa92e5c00b0c5ab
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2026-06-07 16:32:14 +0300

    init

---
+++ src/lib/prisma.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis;
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
