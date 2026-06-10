da692fa2fba499443c644dc10458e129e52475c9
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2026-06-10 16:52:56 +0300

    feat(controllers): add userController - thin layer calling userService

---
+++ src/controllers/userController.js
@@ -0,0 +1,67 @@
+import catchAsync from "../utils/catchAsync.js";
+import {
+  createUser as createUserService,
+  getAllUsers as getAllUsersService,
+  getUserById as getUserByIdService,
+  updateUser as updateUserService,
+  deleteUser as deleteUserService,
+} from "../services/userService.js";
+
+// 1. CREATE USER
+export const createUser = catchAsync(async (req, res, next) => {
+  const newUser = await createUserService(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+// 2. GET ALL USERS WITH FILTERING, SEARCH, SORT, PAGINATION
+export const getAllUsers = catchAsync(async (req, res, next) => {
+  const { meta, users } = await getAllUsersService(req.query);
+
+  res.status(200).json({
+    status: "success",
+    meta,
+    data: {
+      users,
+    },
+  });
+});
+
+// 3. GET USER BY ID
+export const getUserById = catchAsync(async (req, res, next) => {
+  const user = await getUserByIdService(req.params.id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
+
+// 4. UPDATE USER
+export const updateUser = catchAsync(async (req, res, next) => {
+  const updatedUser = await updateUserService(req.params.id, req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+// 5. DELETE USER (SOFT DELETE)
+export const deleteUser = catchAsync(async (req, res, next) => {
+  await deleteUserService(req.params.id);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
